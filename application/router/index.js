c875664fc60887aa66fd3068a913ae2ccbba1262
andreisoroka <unickpro@gmail.com>
2018-03-19 16:46:19 +0300

    [router] fix path index page

--- application/router/index.js
+++ application/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-const IndexPage = () => import(/* webpackChunkName: "IndexPage" */ '../vue/pages/Index/index.vue');
+const IndexPage = () => import(/* webpackChunkName: "IndexPage" */ '../vue/pages/Index/Index.vue');
 const NotFoundPage = () => import(/* webpackChunkName: "NotFoundPage" */ '../vue/pages/NotFound/NotFound.vue');
 
 
